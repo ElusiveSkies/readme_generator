77c9772dcc2a109845ecbf0b4583654b8b3b8380
Casey Twine <casey7twine@gmail.com>
2021-08-08 12:58:23 -0400

    added additional if statements to dynamically create headers

--- README.md
+++ README.md
@@ -1,27 +1,40 @@
-# 
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+# 1
 
 ## Table of Contents
-  [object Object]
+    * [Installation](#Installation)
+    * [Usage](#Usage)
     * [License](#License)
+    * [Contributions](#Contributions)
+    * [Tests](#Tests)
+    * [Questions](#Questions)
 
 ## Description
-  
+
+   2
 
 ## Installation
-  
+
+    3
   
 ## Usage
-  
+
+   4
 
 ## License
-  MIT  
+
+   MIT  
 
 ## Contributions
-  
+
+   5
 
 ## Tests
-  
+
+   6
 
 ## Questions
-  
-  
+
+   7
+   8
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,75 +1,101 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-// function renderLicenseBadge(license) {}
+licenseBadge = [];
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-// function renderLicenseLink(license) {}
+function renderLicenseBadge(responses) {
+  if (responses.license == 'MIT') {
+    licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-// function renderLicenseSection(license) {}
 
 // Created a function to generate markdown for README
 function generateMarkdown(responses) {
-  let contents = {};
+  let contents = [`## Table of Contents`];
 
   if (responses.install !== "") {
     contents += `
     * [Installation](#Installation)`;
+    installContents = `## Installation
+
+    ${responses.install}`;
+  } else {
+    installContents = "";
   }
 
   if (responses.usage !== "") {
     contents += `
     * [Usage](#Usage)`;
+    usageContents = `## Usage
+
+   ${responses.usage}`;
+  } else {
+    usageContents = "";
   }
 
   if (responses.license !== "") {
     contents += `
     * [License](#License)`;
+    licenseContents = `## License
+
+   ${responses.license}`;
   }
 
   if (responses.contribute !== "") {
     contents += `
     * [Contributions](#Contributions)`;
+    contributeContents = `## Contributions
+
+   ${responses.contribute}`;
+  } else {
+    contributeContents = "";
   }
 
   if (responses.test !== "") {
     contents += `
     * [Tests](#Tests)`;
+    testContents = `## Tests
+
+   ${responses.test}`;
+  } else {
+    testContents = "";
   }
 
   if (responses.questions !== "") {
     contents += `
     * [Questions](#Questions)`;
+    questionsContents = `## Questions
+
+   ${responses.questions}
+   ${responses.github}`;
+  } else {
+    questionsContents = "";
   }
 
-  return `# ${responses.title}
+  renderLicenseBadge(responses)
 
-## Table of Contents
-  ${contents}
+
+  return `${licenseBadge}
+# ${responses.title}
+
+${contents}
 
 ## Description
-  ${responses.description}
 
-## Installation
-  ${responses.install}
+   ${responses.description}
+
+${installContents}
   
-## Usage
-  ${responses.usage}
+${usageContents}
 
-## License
-  ${responses.license}  
+${licenseContents}  
 
-## Contributions
-  ${responses.contribute}
+${contributeContents}
 
-## Tests
-  ${responses.test}
+${testContents}
 
-## Questions
-  ${responses.questions}
-  ${responses.github}
+${questionsContents}
+
 `;
 }
 
